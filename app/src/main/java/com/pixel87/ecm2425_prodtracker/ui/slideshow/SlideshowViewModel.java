4dd4328ce942d661cf30d57465d4721cff270937
pixel-87 <149434768+pixel-87@users.noreply.github.com>
2025-03-12 15:18:11 +0000

    first commit

---
+++ app/src/main/java/com/pixel87/ecm2425_prodtracker/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.pixel87.ecm2425_prodtracker.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
